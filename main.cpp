393482563c2c2ab5272ffeb8e0451d3690258284
Anastasia Kapralova <kapralovannnst@yandex.ru>
2020-12-01 13:06:49 +0300

    Перевод сообщений об ошибках
    Для перевода сообщений из библиотеки Qt на русский язык необходим
    файл qtbase_ru.qm (может находиться в папке программы или в
    установочной директории Qt).
    
    Signed-off-by: Anastasia Kapralova <kapralovannnst@yandex.ru>

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include "SeaBattleWindow.h"
 #include <QtWidgets/QApplication>
+#include <QTranslator>
+#include <QLibraryInfo>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // Перевод текстовых строк Qt
+    QTranslator tr;
+    // Поиск файла qtbase_ru.qm в папке с программой
+    if (!tr.load("qtbase_ru.qm", QApplication::applicationDirPath()))
+        // Если не найден, то загрузить из установленной библиотеки Qt
+        tr.load("qtbase_ru.qm", QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    a.installTranslator(&tr);
+
     SeaBattleWindow w;
     w.show();
     return a.exec();
